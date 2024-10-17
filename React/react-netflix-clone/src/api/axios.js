1fa4723472b1b57e924e084f2400b7233f13d277
magik99 <hojun0810@naver.com>
2024-10-17 23:16:10 +0900

    섹션5 넷플릭스 앱 만들기 시작 완료

---
+++ React/react-netflix-clone/src/api/axios.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import API_KEY from '../key';
+
+const instance = axios.create({
+  baseURL: 'https://api.themoviedb.org/3',
+  params: {
+    api_key: `${API_KEY}`,
+    language: 'ko-KR',
+  },
+});
+
+export default instance;
